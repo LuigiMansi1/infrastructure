0cb8b9ed7a4113afb0cea693e9065475a6398455
po-khmel <khmelevskayapv@gmail.com>
2023-08-06 21:32:44 +0200

    add web ports for rabbitmq

--- instance_rabbitmq.tf
+++ instance_rabbitmq.tf
@@ -3,7 +3,7 @@ resource "openstack_compute_instance_v2" "rabbitmq" {
   image_name      = data.openstack_images_image_v2.rocky_image.name
   flavor_name     = var.flavors.rabbitmq
   key_pair        = openstack_compute_keypair_v2.cloud.name
-  security_groups = ["public-ssh", "public-ping", "public-amqp", "egress"]
+  security_groups = ["public-ssh", "public-ping", "public-amqp", "public-web", "egress"]
 
   # network {
   #   name = var.public_network.name
