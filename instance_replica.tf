185c47cd7b62ac46fb210d292a402117fc480c11
LuigiMansi1 <101326982+LuigiMansi1@users.noreply.github.com>
2024-10-30 18:51:44 +0100

    Update instance_replica.tf

--- instance_replica.tf
+++ instance_replica.tf
@@ -8,4 +8,27 @@ resource "openstack_compute_instance_v2" "replica" {
   network {
     name = var.private_network.name
   }
+  block_device {
+    uuid                  = data.openstack_images_image_v2.rocky_image.id
+    source_type           = "image"
+    destination_type      = "local"
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+  block_device {
+    uuid                  = openstack_blockstorage_volume_v3.replica_volume.id
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = -1
+    delete_on_termination = true
+  }
+  user_data = file("files/create_psql.sh")
+}
+
+resource "openstack_blockstorage_volume_v3" "replica_volume" {
+  name        = var.replica.name
+  description = var.replica.description
+  volume_type = var.replica.volume_type
+  size        = var.replica.disk_size
 }
