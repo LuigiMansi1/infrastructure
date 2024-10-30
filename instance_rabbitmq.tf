46b3731fd3f0e6892c836b1d353cca14b829d684
LuigiMansi1 <101326982+LuigiMansi1@users.noreply.github.com>
2024-10-30 18:50:51 +0100

    Update instance_rabbitmq.tf

--- instance_rabbitmq.tf
+++ instance_rabbitmq.tf
@@ -5,20 +5,12 @@ resource "openstack_compute_instance_v2" "rabbitmq" {
   key_pair        = openstack_compute_keypair_v2.cloud.name
   security_groups = ["public-ssh", "public-ping", "public-amqp", "egress"]
 
-  # network {
-  #   name = var.public_network.name
-  # }
+  network {
+    name = var.public_network.name
+  }
   network {
     name = var.private_network.name
   }
 }
 
-resource "openstack_networking_floatingip_v2" "rabbitmq_fl_ip" {
-  pool = var.public_network.name
-}
-
-resource "openstack_compute_floatingip_associate_v2" "rabbitmq_fl_ip" {
-  floating_ip = openstack_networking_floatingip_v2.rabbitmq_fl_ip.address
-  instance_id = openstack_compute_instance_v2.rabbitmq.id
-}
 
