6da971ca1176f3cb6445aadcc8e63c33dd9ce256
LuigiMansi1 <101326982+LuigiMansi1@users.noreply.github.com>
2024-10-30 18:48:12 +0100

    Update instance_galaxy.tf

--- instance_galaxy.tf
+++ instance_galaxy.tf
@@ -5,22 +5,73 @@ resource "openstack_compute_instance_v2" "galaxy" {
   key_pair        = openstack_compute_keypair_v2.cloud.name
   security_groups = ["egress", "public-web", "public-ssh", "public-ping", "public-condor", "public-tus"]
 
-  # network {
-  #   name = var.public_network.name
-  # }
+  network {
+    name = var.public_network.name
+}
 
   network {
     name = var.private_network.name
   }
+  
+  block_device {
+    uuid                  = data.openstack_images_image_v2.rocky_image.id
+    source_type           = "image"
+    destination_type      = "local"
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+  #Volume for /data/share
+  block_device {
+    uuid                  = openstack_blockstorage_volume_v3.share_volume.id
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = -1
+    delete_on_termination = true
+  }
+
+  #Volume for /data/tool_deps
+  block_device {
+    uuid                  = openstack_blockstorage_volume_v3.tool_deps_volume.id
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = -1
+    delete_on_termination = true
+  }
+
+  #Volume for /opt/galaxy
+  block_device {
+    uuid                  = openstack_blockstorage_volume_v3.galaxy_opt_volume.id
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = -1
+    delete_on_termination = true
+  }
+
+  user_data = file("${path.module}/files/mount_volumes_galaxy.sh")
+
 }
 
-resource "openstack_networking_floatingip_v2" "galaxy_fl_ip" {
-  pool = var.public_network.name
+
+resource "openstack_blockstorage_volume_v3" "share_volume" {
+  name        = var.galaxy_data.name
+  description = var.galaxy_data.description
+  volume_type = var.galaxy_data.volume_type
+  size        = var.galaxy_data.disk_size
 }
 
-resource "openstack_compute_floatingip_associate_v2" "galaxy_fl_ip" {
-  floating_ip = openstack_networking_floatingip_v2.galaxy_fl_ip.address
-  instance_id = openstack_compute_instance_v2.galaxy.id
-  fixed_ip    = openstack_compute_instance_v2.galaxy.network.0.fixed_ip_v4
+
+resource "openstack_blockstorage_volume_v3" "tool_deps_volume" {
+  name        = var.galaxy_conda.name
+  description = var.galaxy_conda.description
+  volume_type = var.galaxy_conda.volume_type
+  size        = var.galaxy_conda.disk_size
 }
 
+
+resource "openstack_blockstorage_volume_v3" "galaxy_opt_volume" {
+  name        = var.galaxy_galaxy.name
+  description = var.galaxy_galaxy.description
+  volume_type = var.galaxy_galaxy.volume_type
+  size        = var.galaxy_galaxy.disk_size
+}
