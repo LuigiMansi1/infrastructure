1878265554a1cffeeca0a0ab5ad9b8162e001374
LuigiMansi1 <101326982+LuigiMansi1@users.noreply.github.com>
2024-10-30 18:58:22 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,11 @@
-# private_ip
+# public_ip
 output "galaxy" {
   value = openstack_compute_instance_v2.galaxy.network[0].fixed_ip_v4
 }
 
-output "galaxy_fl_ip" {
-  value = openstack_networking_floatingip_v2.galaxy_fl_ip.address
+# private_ip
+output "galaxy" {
+  value = openstack_compute_instance_v2.galaxy.network[1].fixed_ip_v4
 }
 
 # private_ip
@@ -32,20 +33,22 @@ output "backup" {
   value = openstack_compute_instance_v2.backup.network[0].fixed_ip_v4
 }
 
-# private_ip
+# public_ip
 output "rabbitmq" {
   value = openstack_compute_instance_v2.rabbitmq.network[0].fixed_ip_v4
 }
 
-output "rabbitmq_fl_ip" {
-  value = openstack_networking_floatingip_v2.rabbitmq_fl_ip.address
+# private_ip
+output "rabbitmq" {
+  value = openstack_compute_instance_v2.rabbitmq.network[1].fixed_ip_v4
 }
 
-# private_ip
+# public_ip
 output "central-manager" {
   value = openstack_compute_instance_v2.vgcn-cm.network[0].fixed_ip_v4
 }
 
-output "central-manager_fl_ip" {
-  value = openstack_networking_floatingip_v2.vgcn_fl_ip.address
+# private_ip
+output "central-manager" {
+  value = openstack_compute_instance_v2.vgcn-cm.network[1].fixed_ip_v4
 }
