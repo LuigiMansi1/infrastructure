57c17687af6745336499ccf62b2fba8c8e937268
LuigiMansi1 <101326982+LuigiMansi1@users.noreply.github.com>
2024-10-30 18:42:57 +0100

    Update instance_backup.tf

--- instance_backup.tf
+++ instance_backup.tf
@@ -8,4 +8,32 @@ resource "openstack_compute_instance_v2" "backup" {
   network {
     name = var.private_network.name
   }
+
+
+  block_device {
+    uuid                  = data.openstack_images_image_v2.rocky_image.id
+    source_type           = "image"
+    destination_type      = "local"
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+  block_device {
+    uuid                  = openstack_blockstorage_volume_v3.backup_volume.id
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = -1
+    delete_on_termination = true
+  }
+
+  user_data = file("files/create_data.sh") 
+
+}
+
+
+resource "openstack_blockstorage_volume_v3" "backup_volume" {
+  name        = var.backup.name
+  description = var.backup.description
+  volume_type = var.backup.volume_type
+  size        = var.backup.disk_size
 }
