0c723119e423cc0bba5df41111cd1be62b01f18e
LuigiMansi1 <101326982+LuigiMansi1@users.noreply.github.com>
2024-10-30 18:48:49 +0100

    Update instance_master.tf

--- instance_master.tf
+++ instance_master.tf
@@ -13,13 +13,3 @@ resource "openstack_compute_instance_v2" "mastervm" {
     name = var.private_network.name
   }
 }
-
-resource "openstack_networking_floatingip_v2" "mastervm_fl_ip" {
-  pool = var.public_network.name
-}
-
-resource "openstack_compute_floatingip_associate_v2" "mastervm_fl_ip" {
-  floating_ip = openstack_networking_floatingip_v2.mastervm_fl_ip.address
-  instance_id = openstack_compute_instance_v2.mastervm.id
-  fixed_ip    = openstack_compute_instance_v2.mastervm.network.0.fixed_ip_v4
-}
